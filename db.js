1d18ce95062448d737cdaeb613e6eeb453d0ce79
Lucas Moreira <lucas_moreira@outlook.com>
2024-04-30 07:59:45 -0300

    Alteração no banco de dados

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 import 'dotenv/config'
 import postgres from 'postgres'
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
 
 export const sql = postgres(URL, { ssl: 'require' });
