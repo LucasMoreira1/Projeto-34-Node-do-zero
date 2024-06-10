998d1a2956fe8ff025d9b62d2530409fe706349e
Lucas Moreira <lucas_moreira@outlook.com>
2024-06-10 15:46:33 -0300

    Adicionado integração WHATSAPP e GEMINI

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ import { DatabasePostgres } from './database-postgres.js'
 import fs from 'fs'
 import Docxtemplater from 'docxtemplater'
 import JSZip from 'jszip'
+import 'dotenv/config'
+import axios from 'axios'
+import formbody from '@fastify/formbody' // Importação do plugin
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const server = fastify()
 
+// Registro dos plugins
 server.register(fastifyCors, {
-    // Configurações do CORS
     origin: '*', // Permitir todas as origens (não recomendado para produção)
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-  });
+})
+
+server.register(formbody) // Registro do plugin formbody separadamente
+
 
 const database = new DatabasePostgres()
 
@@ -429,9 +437,172 @@ server.post('/gerar-docx', async (request, reply) => {
       }
     });
 
-////////////////////////////////////////////////////////////////
+
+//
+// Whatsapp
+//
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN } = process.env;
+
+const chatHistory = {}; // Armazenar o histórico das conversas
+
+server.post('/webhook', async (request, reply) => {
+    // console.log('Incoming webhook message:', JSON.stringify(request.body, null, 2));
+
+    const message = request.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+    // console.log(message)
+
+    if (message?.type === 'text') {
+        const business_phone_number_id = request.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+        const fromNumber = message.from;
+        const senderName = request.body.entry?.[0].changes?.[0].value?.contacts?.[0].profile?.name; // Extrai o nome do remetente da mensagem
+        const messageText = message.text.body;
+
+        // Adiciona histórico de conversa se não existir
+        if (!chatHistory[fromNumber]) {
+            chatHistory[fromNumber] = [
+                {
+                    role: "user",
+                    // parts: [{ text: ``}],
+                    parts: [{ text: `Seu nome será Ratatuile e é um assistente virtual para atendimento da PestClean. Na introdução, sempre informar o nome da pessoa, se houver. Preciso que seja bem amigável e use emojis na conversa, sem exagerar. É uma empresa de dedetização. Seus principais serviços são desratização, dedetização, descupinização e desalojamento de pombos. Também presta serviços como limpeza de caixa d'agua, caixa de gordura, pintura de caixa d'agua entre outros. Para mais informações e orçamentos entrar em contato com o Bruno Moreira (12) 98147-4513. Seu endereço é Av. Bruno Decária, 277 - Jardim Santa Maria, Jacareí - SP.` }],
+                }
+            ];
+        }
+
+        // Adiciona a mensagem recebida ao histórico
+        chatHistory[fromNumber].push({
+            role: "user",
+            parts: [{ text: messageText }],
+        });
+
+        // Integração com a função resposta_gemini
+        try {
+            const geminiResponse = await resposta_gemini(chatHistory[fromNumber], senderName, fromNumber); // Passa o histórico atualizado como parâmetro
+            // const geminiText = geminiResponse.text; // Extrai o texto da resposta de gemini
+
+            // Adiciona a resposta de Gemini ao histórico
+            chatHistory[fromNumber].push({
+                role: "model",
+                parts: [{ text: geminiResponse }],
+            });
+
+            await axios({
+                method: 'POST',
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                    Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+                },
+                data: {
+                    messaging_product: 'whatsapp',
+                    to: fromNumber,
+                    text: { body: geminiResponse }, // Envia a resposta de gemini
+                    context: {
+                        message_id: message.id,
+                    },
+                },
+            });
+
+            await axios({
+                method: 'POST',
+                url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
+                headers: {
+                    Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+                },
+                data: {
+                    messaging_product: 'whatsapp',
+                    status: 'read',
+                    message_id: message.id,
+                },
+            });
+        } catch (error) {
+            console.error('Erro ao processar a mensagem:', error);
+        }
+    }
+
+    reply.code(200).send();
+});
+
+server.get('/webhook', (request, reply) => {
+    const mode = request.query['hub.mode']
+    const token = request.query['hub.verify_token']
+    const challenge = request.query['hub.challenge']
+
+    // check the mode and token sent are correct
+    if (mode === 'subscribe' && token === WEBHOOK_VERIFY_TOKEN) {
+        // respond with 200 OK and challenge token from the request
+        reply.code(200).send(challenge)
+        console.log('Webhook verified successfully!')
+    } else {
+        // respond with '403 Forbidden' if verify tokens do not match
+        reply.code(403).send()
+    }
+})
+
+server.get('/', (request, reply) => {
+    reply.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`)
+})
+
+//
+// GEMINI
+//
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const generationConfig = {
+    stopSequences: ["red"],
+    maxOutputTokens: 200,
+    temperature: 0.9,
+    topP: 0.1,
+    topK: 16,
+};
+
+// The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig });
+
+async function resposta_gemini(history, senderName, fromNumber) {
+    try {
+        // Adiciona um histórico inicial, se necessário
+        if (!history.find(item => item.role === "model")) {
+            history.push({
+                role: "model",
+                parts: [{ text: `Olá, ${senderName}!` }],
+            });
+        }
+
+        const chat = model.startChat({ history });
+
+        const result = await chat.sendMessage(history[history.length - 1].parts[0].text);
+
+        // Utiliza a mensagem recebida como prompt para a geração de conteúdo
+        // const result = await model.generateContent(prompt);
+        
+        // Extrai a resposta do resultado da geração de conteúdo
+        const response = await result.response;
+        
+        // Extrai o texto da resposta
+        const text = await response.text();
+
+        console.log(history)
+        console.log(senderName, " - ", fromNumber);
+        console.log(text);
+        // Retorna o texto da resposta
+        return text;
+    } catch (error) {
+        console.error('Erro ao gerar resposta com Gemini:', error);
+        throw error;
+    }
+}
+
+////////////////////////////////////
 
 server.listen({
     host: '0.0.0.0',
     port: process.env.PORT ?? 3333,
+}, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server is listening on ${address}`)
 })
